3d450503a9db53c16ed1cc820f2d5e8e9b17511b
Mohamed Omran <mohamedgomran@gmail.com>
2019-04-17 13:40:35 +0200

    Implement removeWord, use messages, use simple state, add contextmenu

--- content.js
+++ content.js
@@ -1,13 +1,28 @@
-document.addEventListener('DOMNodeInserted', () => {
-  chrome.storage.sync.get(["words"], res => {
-    const currentWords = "{}" !== JSON.stringify(res) ? JSON.parse(res.words) : [];
-    const allPosts = Array.from(document.getElementsByClassName('_5jmm'));
-    allPosts.map(post => {
-      currentWords.forEach(word => {
-        if (post.textContent.includes(word)) {
+const state = {
+  postsLength: 0,
+  wordsLength: 0,
+};
+
+const remove = () => {
+  const allPosts = Array.from(document.getElementsByClassName('_5jmm'));
+  chrome.storage.sync.get(['words'], (res) => {
+    const currentWords = JSON.stringify(res) !== '{}' ? JSON.parse(res.words) : [];
+    if (allPosts.length === state.postsLength && currentWords.length === state.wordsLength) return;
+    state.postsLength = allPosts.length;
+    state.wordsLength = currentWords.length;
+    allPosts.forEach((post) => {
+      currentWords.forEach((word) => {
+        if (post.textContent.toLowerCase().includes(word.toLowerCase())) {
           post.style.display = 'none';
+          return undefined; // critical return
         }
-      })
-    })
+      });
+    });
   });
-})
+};
+
+chrome.runtime.onMessage.addListener(({ method }) => {
+  if (method === 'remove') remove();
+});
+
+document.addEventListener('scroll', remove);
